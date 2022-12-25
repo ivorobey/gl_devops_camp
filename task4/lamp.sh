6e025105aa38b5dae68ddae05485f5845f7e6de3
Roman <42977616+ivorobey@users.noreply.github.com>
2022-12-26 00:06:46 +0200

    Update lamp.sh

--- task4/lamp.sh
+++ task4/lamp.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+sudo apt-get update -y
+sudo apt install -y apache2
+
 sudo mkdir -p /var/www/html/
 sudo echo "<h2>GCP lamp by sparrow<h2>" > /var/www/html/index.html
 
-sudo apt-get update -y
-sudo apt-get install apache2 php libapache2-mod-php
-sudo apt-get install mariadb-server php php-mysql
-sudo apt-get install wget -y
-sudo wget https://wordpress.org/latest.tar.gz
-sudo tar xzvf latest.tar.gz
-sudo chown -R www-data:www-data /var/www/html/wordpress/
+sudo apt install -y mariadb-server
+sudo apt install -y php libapache2-mod-php php-mysql
+sudo touch /var/www/html/phpinfo.php
+cat <<EOF > /var/www/html/phpinfo.php
+ <?php phpinfo(); ?>
+EOF
 sudo service apache2 restart
