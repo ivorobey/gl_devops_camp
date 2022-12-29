610b2a127d221b2e5176e29c05258df934c3c6d6
Roman <42977616+ivorobey@users.noreply.github.com>
2022-12-30 00:45:15 +0200

    Update main.tf

--- task5/terraform/main.tf
+++ task5/terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = ""                     # insert your access_key here
+  access_key = "" # insert your access_key here
   secret_key = "" # insert your secret_key here  
   region     = "us-east-1"
 }
