0730b6ec637250d372fae1dfe84631edbc92a76c
Roman <42977616+ivorobey@users.noreply.github.com>
2023-01-05 21:32:22 +0200

    Update data_grafana.sh

--- task6/data_grafana.sh
+++ task6/data_grafana.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common 
-
-curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add - 
-
-sudo add-apt-repository \
-    "deb https://apt.dockerproject.org/repo/ \
-    ubuntu-$(lsb_release -cs) \
-    main" 
-
 sudo apt-get update
-sudo apt-get -y install docker-engine 
-
+sudo apt-get install -y cloud-utils apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
 # add current user to docker group so there is no need to use sudo when running docker
 sudo usermod -aG docker $(whoami)
 
-sleep 20
+sudo docker pull grafana/grafana
 
-docker pull grafana/grafana
+sudo docker run -d --name=grafana -p 3000:3000 grafana/grafana
 
-docker run -d --name=grafana -p 80:3000 grafana/grafana
+
+#Another way to install Grafana
+
+# sudo apt-get update -y
+# apt-get upgrade -y
+# sudo apt-get install gnupg2 curl wget git software-properties-common -y
+# sudo curl https://packages.grafana.com/gpg.key | apt-key add -
+# add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+# sudo apt-get update -y
+# sudo apt-get install grafana -y
+# sudo systemctl start grafana-server
+# sudo systemctl enable grafana-server
