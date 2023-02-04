3ce1c5e6636ba66ef2ccfe60c6ed747ebfa5a401
ivorobey <romvorobey@gmail.com>
2023-02-04 19:29:26 +0200

    fix sxript

--- task9/jenkins.sh
+++ task9/jenkins.sh
@@ -39,6 +39,18 @@ COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
 RUN jenkins-plugin-cli -f /usr/share/jenkins/ref/plugins.txt
 COPY casc.yaml /var/jenkins_home/casc.yaml" > Dockerfile
 # -------------
-docker build -t jenkins:1.0 .
-# -------------
-docker run --name first-jenkins -d -p 8080:8080 jenkins:1.0
+sudo apt-get update
+sudo apt-get install -y cloud-utils apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+# add current user to docker group so there is no need to use sudo when running docker
+sudo usermod -aG docker $(whoami)
+
+sudo docker build -t jenkins .
+
+sudo docker run --name myjenkins -d -p 8080:8080 jenkins
